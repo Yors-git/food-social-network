d3b20353fb52e0d1c134befe3741b5f0d5e7d791
Yors <jatbribiesca@gmail.com>
2021-01-12 09:53:32 -0600

    add cloudinary to manage images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :opinions, class_name: 'Opinion', foreign_key: 'author_id'
 
+  has_one_attached :photo
+
   has_many :followers, class_name: 'Followship', foreign_key: 'user_id'
   has_many :inverse_followers, class_name: 'Followship', foreign_key: 'follower_id'
 end
