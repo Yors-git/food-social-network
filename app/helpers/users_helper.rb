9acc37e85a1892f7b1fadd09ac585a9e9042c2e6
Yors <jatbribiesca@gmail.com>
2021-01-13 09:44:38 -0600

    add coverimage style and fix styles from all project

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+  def check_profile_photo(user)
+    if user.photo.attached?
+      image_tag(url_for(user.photo), alt:'user avatar', style:'height:60px')
+    else
+      image_tag(url_for('https://res.cloudinary.com/dzxasrolj/image/upload/v1610477078/burger_v84wnz.png'), alt:'user avatar', style:'height:60px')
+    end
+  end
+
+  def check_coverimage(user)
+    if user.photo.attached?
+      cl_image_tag(url_for(user.coverimage), alt:'user cover image')
+    else
+      cl_image_tag(url_for('https://res.cloudinary.com/dzxasrolj/image/upload/v1610477078/burger_v84wnz.png'), alt:'user cover image')
+    end
+  end
+
+  def show_background_image
+    url = "url('https://res.cloudinary.com/dzxasrolj/image/upload/v1610477078/" + "#{@user.coverimage.key}')" 
+    content_tag(:div, '', class: 'cover-image', style: "background-image:#{url}")
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :opinions, class_name: 'Opinion', foreign_key: 'author_id'
 
   has_one_attached :photo
+  has_one_attached :coverimage
 
   has_many :followers, class_name: 'Followship', foreign_key: 'user_id'
   has_many :inverse_followers, class_name: 'Followship', foreign_key: 'follower_id'
@@ -10,5 +11,4 @@ class User < ApplicationRecord
     followed_by_ids = user.followers.all.map { |fllr| fllr.follower_id if fllr.user_id == user.id }
     followed_by = User.where(id: followed_by_ids)
   end
-
 end
