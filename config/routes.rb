4d354c2f4896421040045dbe35e68f6d283410d7
Yors <jatbribiesca@gmail.com>
2021-01-16 15:49:29 -0600

    remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'delete', to: 'sessions#destroy'
-  get 'welcome', to: 'sessions#welcome'
-  resources :users
-  resources :users do
+  resources :users, only: [:index, :new, :create, :show] do
     member do
       get :create_follower
       get :delete_follower
     end
   end
-  resources :opinions
+  resources :opinions, only: [:index, :new, :create]
 
   root 'opinions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
