53b8eb28d77e1f92b4f9c1415ae86be0dfa10de2
Yors <jatbribiesca@gmail.com>
2021-01-15 21:26:07 -0600

    change db in all enviroments to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_01_12_023351) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 2021_01_12_023351) do
   end
 
   create_table "followships", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "follower_id"
+    t.bigint "user_id"
+    t.bigint "follower_id"
     t.boolean "blocked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_023351) do
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "author_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_opinions_on_author_id"
   end
 
