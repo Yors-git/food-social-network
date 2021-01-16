e6b32bb8f2fc82c4fbdb40c1439898b40121a1f2
Yors <jatbribiesca@gmail.com>
2021-01-16 14:22:47 -0600

    add shoulda matchers to check uniqueness and length in models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,12 +20,12 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it 'check user username presence' do
-    expect(inv_user).to_not be_valid
-  end
-
-  it 'check user fullname presence' do
-    expect(inv_user).to_not be_valid
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:fullname) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:username).is_at_least(3) }
+    it { should validate_length_of(:fullname).is_at_least(5) }
   end
 
   describe 'associations' do
